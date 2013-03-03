3cac9e17fbcf84fa9fe849913fa2122f3d9dedf8
Mirko Kiefer <mail@mirkokiefer.com>
2013-03-03 16:07:41 +0100

    add mongoose.cmake

---
+++ deps/mongoose.cmake
@@ -0,0 +1,14 @@
+
+ExternalProject_Add(project_mongoose
+  URL https://github.com/mirkok/mongoose/archive/3.7-cmake.tar.gz
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/mongoose
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+)
+
+ExternalProject_Get_Property(project_mongoose install_dir)
+
+include_directories(${install_dir}/include)
+
+add_library(mongoose STATIC IMPORTED GLOBAL)
+set_property(TARGET mongoose PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libmongoose.a)
+add_dependencies(mongoose project_mongoose)
