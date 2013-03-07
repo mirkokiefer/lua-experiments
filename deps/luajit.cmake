149361db21508dbde3c7269bc49a017fb87838f4
Mirko Kiefer <mail@mirkokiefer.com>
2013-03-07 19:36:53 +0200

    rename luajit project

--- deps/luajit.cmake
+++ deps/luajit.cmake
@@ -1,5 +1,5 @@
 
-ExternalProject_Add(project_lua
+ExternalProject_Add(project_luajit
   URL http://luajit.org/download/LuaJIT-2.0.1.tar.gz
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/luajit
   CONFIGURE_COMMAND ""
@@ -8,10 +8,10 @@ ExternalProject_Add(project_lua
   BUILD_IN_SOURCE 1
 )
 
-ExternalProject_Get_Property(project_lua install_dir)
+ExternalProject_Get_Property(project_luajit install_dir)
 
 include_directories(${install_dir}/include)
 
 add_library(lua STATIC IMPORTED)
 set_property(TARGET lua PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libluajit-5.1.a)
-add_dependencies(lua project_lua)
+add_dependencies(lua project_luajit)
