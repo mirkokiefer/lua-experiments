7fe1a64125690e9fdac2a0fe6a141f5704c636b3
Mirko Kiefer <mail@mirkokiefer.com>
2013-03-09 10:52:58 +0200

    update luajit url

--- deps/luajit.cmake
+++ deps/luajit.cmake
@@ -1,6 +1,6 @@
 
 ExternalProject_Add(project_luajit
-  URL https://github.com/mirkok/luajit.cmake/archive/v2.0.1.tar.gz
+  URL https://github.com/mirkok/luajit.cmake/archive/v2.tar.gz
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/lua
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/lua
 )
